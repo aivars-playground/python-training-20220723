be7293bc66bd397c899872858264a584fd5f612c
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2022-07-27 14:17:22 +0000

    refactoring

--- pgbackup/src/pgbackup/cli.py
+++ pgbackup/src/pgbackup/cli.py
@@ -1,4 +1,6 @@
 from argparse import Action, ArgumentParser
+from http import client
+import time
 
 known_drivers = ['local', 's3']
 
@@ -20,3 +22,18 @@ def create_parser():
 		required=True,
 		action=DriverAction)
 	return parser
+
+def main():
+	import boto3
+	from pgbackup import pgdump, storage
+
+	args = create_parser().parse_args()
+	dump = pgdump.dump(args.url)
+	if args.driver == 's3':
+		client = boto3.client('s3')
+		timestamp = time.strftime('%Y-%m-%dT%H:%M', time.localtime())
+		filename = pgdump.dump_file_name(args.url, timestamp)
+		storage.s3(client, dump.stdout, args.destination, filename)
+	else:
+		outfile = open(args.destination, 'wb')
+		storage.local(dump.stdout, outfile)
