ab53bf6b994e28a08e12fb84a021d6be6fe7a494
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2022-07-28 15:43:15 +0000

    init

---
+++ pytickets/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask, render_template
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('SECRET_KEY', default='dev')
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return "page not found", 404
+
+    return app
+
