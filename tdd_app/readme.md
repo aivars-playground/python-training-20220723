719be0adf67c2346fcce94b279674673ade24118
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2022-08-10 18:53:46 +0000

    adding tests

--- tdd_app/readme.md
+++ tdd_app/readme.md
@@ -1,6 +1,18 @@
 starting development
 ---
-
 ```bash
 make install
+```
+
+testing
+---
+```bash
+make test
+```
+
+if app is installed
+---
+```bash
+$ python -m tddapp
+> HELLO
 ```
---
+++ tdd_app/tests/test_tddapp.py
@@ -0,0 +1,30 @@
+import pytest
+from tddapp import cli
+
+
+def test_app_without_params():
+    """
+    Without parameters app will exit
+    """
+    with pytest.raises(SystemExit):
+        parser = cli.create_parser()
+        parser.parse_args()
+
+
+def test_app_with_source_file_no_filename():
+    """
+    With source and target file (without location)
+    """
+    with pytest.raises(SystemExit):
+        parser = cli.create_parser()
+        parser.parse_args([".", "--target"])
+
+
+def test_app_with_source_and_file_target():
+    """
+    With source and default target
+    """
+    parser = cli.create_parser()
+    args = parser.parse_args([".", "--target", ".ignoreme/out.csv"])
+    assert args.source == "."
+    assert args.target == ".ignoreme/out.csv"
